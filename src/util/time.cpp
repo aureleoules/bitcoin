12bdef9d0a547ce147fb60fcff0c210c01675f2a
Aurèle Oulès <aurele@oules.com>
2022-09-29 10:18:56 +0200

    refactor: time

--- src/util/time.cpp
+++ src/util/time.cpp
@@ -144,16 +144,23 @@ std::string FormatISO8601Date(int64_t nTime) {
 
 int64_t ParseISO8601DateTime(const std::string& str)
 {
-    static const boost::posix_time::ptime epoch = boost::posix_time::from_time_t(0);
-    static const std::locale loc(std::locale::classic(),
-        new boost::posix_time::time_input_facet("%Y-%m-%dT%H:%M:%SZ"));
-    std::istringstream iss(str);
-    iss.imbue(loc);
-    boost::posix_time::ptime ptime(boost::date_time::not_a_date_time);
-    iss >> ptime;
-    if (ptime.is_not_a_date_time() || epoch > ptime)
+    // static const boost::posix_time::ptime epoch = boost::posix_time::from_time_t(0);
+    // static const std::locale loc(std::locale::classic(),
+    //     new boost::posix_time::time_input_facet("%Y-%m-%dT%H:%M:%SZ"));
+    // std::istringstream iss(str);
+    // iss.imbue(loc);
+    // boost::posix_time::ptime ptime(boost::date_time::not_a_date_time);
+    // iss >> ptime;
+    // if (ptime.is_not_a_date_time() || epoch > ptime)
+    //     return 0;
+    // return (ptime - epoch).total_seconds();
+
+    // alternative without boosrt
+    struct tm tm;
+    if (strptime(str.c_str(), "%Y-%m-%dT%H:%M:%SZ", &tm) == nullptr) {
         return 0;
-    return (ptime - epoch).total_seconds();
+    }
+    return timegm(&tm);
 }
 
 struct timeval MillisToTimeval(int64_t nTimeout)
